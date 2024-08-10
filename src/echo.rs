313fb365caf43f795fd683a547e7a4e5eb45407c
arturgc <artur.garcia.costa@gmail.com>
2024-08-10 14:10:15 -0300

    refactor: implements enums and methods for echo

--- src/echo.rs
+++ src/echo.rs
@@ -1,81 +1,117 @@
 use std::env::Args;
 
-pub fn run(mut args: Args) {
-    if args.len() == 0 {
-        panic!("\"string\" argument expected");
-    }
-
-    if args.len() == 1 {
-        let string = args.next().unwrap();
-
-        return println!("{}", process_no_flag(string));
-    }
-
-    if args.len() == 2 {
-        let option = args.next().unwrap();
-        let string = args.next().unwrap();
-
-        return match &option[..] {
-            "-e" => println!("{}", process_e_flag(string)),
-            _ => panic!("Option not implemented"),
-        };
-    }
-
-    panic!("Not implemented")
+enum Flag {
+    E,
 }
 
-fn process_no_flag(string: String) -> String {
-    format!("{string}")
-}
-
-fn process_e_flag(string: String) -> String {
-    let mut result = String::new();
-    let mut scape = false;
-
-    for character in string.chars() {
-        if character == '\\' {
-            scape = true;
-            continue;
+impl Flag {
+    fn new(flag: String) -> Self {
+        match &flag[..] {
+            "-e" => Self::E,
+            _ => panic!("Flag not implemented"),
         }
-
-        if scape {
-            if character == 'b' && result.ends_with(' ') {
-                result.pop();
-            }
-
-            if character == 'n' {
-                result.push_str("\n");
-            }
-
-            if character == 't' {
-                result.push_str("\t");
-            }
-
-            if character == 'v' {
-                let spaces = match result.split("\n").last() {
-                    Some(line) => format!("\n{}", " ".repeat(line.len())),
-                    None => String::new(),
-                };
-
-                result.push_str(&spaces);
-            }
-
-            if character == 'r' {
-                result.clear();
-            }
-
-            if character == 'c' {
-                return format!("{result}");
-            }
-
-            scape = false;
-            continue;
-        }
-
-        result.push(character);
     }
 
-    format!("{result}")
+    fn process(string: String) -> String {
+        format!("{string}")
+    }
+
+    fn process_e(string: String) -> String {
+        let mut result = String::new();
+        let mut handle_as_scape_key = false;
+
+        for character in string.chars() {
+            if character == '\\' {
+                handle_as_scape_key = true;
+                continue;
+            }
+
+            if handle_as_scape_key {
+                if character == 'b' && result.ends_with(' ') {
+                    result.pop();
+                }
+
+                if character == 'n' {
+                    result.push_str("\n");
+                }
+
+                if character == '\\' {
+                    result.push_str("\\");
+                }
+
+                if character == 't' {
+                    result.push_str("\t");
+                }
+
+                if character == 'v' {
+                    let spaces = match result.split("\n").last() {
+                        Some(line) => format!("\n{}", " ".repeat(line.len())),
+                        None => String::new(),
+                    };
+
+                    result.push_str(&spaces);
+                }
+
+                if character == 'r' {
+                    result.clear();
+                }
+
+                if character == 'c' {
+                    return format!("{result}");
+                }
+
+                handle_as_scape_key = false;
+
+                continue;
+            }
+
+            result.push(character);
+        }
+
+        format!("{result}")
+    }
+}
+
+enum EchoImpl {
+    Show { string: String },
+    ShowFormatted { flag: Flag, string: String },
+}
+
+impl EchoImpl {
+    fn new(args: Args) -> Self {
+        match args.len() {
+            0 => panic!("\"string\" argument expected"),
+            1 => Self::new_show(args),
+            2 => Self::new_show_formatted(args),
+            _ => panic!("Not implemented"),
+        }
+    }
+
+    fn new_show(mut args: Args) -> Self {
+        Self::Show {
+            string: args.next().unwrap(),
+        }
+    }
+
+    fn new_show_formatted(mut args: Args) -> Self {
+        Self::ShowFormatted {
+            flag: Flag::new(args.next().unwrap()),
+            string: args.next().unwrap(),
+        }
+    }
+}
+
+pub fn run(args: Args) {
+    let implementation = EchoImpl::new(args);
+
+    let result = match implementation {
+        EchoImpl::Show { string } => Flag::process(string),
+        EchoImpl::ShowFormatted { flag, string } => match flag {
+            Flag::E => Flag::process_e(string),
+        },
+    };
+
+    println!("{result}")
 }
 
 #[cfg(test)]
@@ -85,7 +121,7 @@ mod tests {
     #[test]
     fn no_flag_test() {
         assert_eq!(
-            process_no_flag(String::from("Geeks for Geeks")),
+            Flag::process(String::from("Geeks for Geeks")),
             "Geeks for Geeks"
         );
     }
@@ -93,20 +129,23 @@ mod tests {
     #[test]
     fn e_flag_test_b() {
         assert_eq!(
-            process_e_flag(String::from("Geeks \\bfor \\bGeeks")),
+            Flag::process_e(String::from("Geeks \\bfor \\bGeeks")),
             "GeeksforGeeks"
         );
     }
 
     #[test]
     fn e_flag_test_c() {
-        assert_eq!(process_e_flag(String::from("Geeks \\cfor Geeks")), "Geeks ");
+        assert_eq!(
+            Flag::process_e(String::from("Geeks \\cfor Geeks")),
+            "Geeks "
+        );
     }
 
     #[test]
     fn e_flag_test_n() {
         assert_eq!(
-            process_e_flag(String::from("Geeks \\nfor \\nGeeks")),
+            Flag::process_e(String::from("Geeks \\nfor \\nGeeks")),
             "Geeks \nfor \nGeeks"
         );
     }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ use rs_cli_core::{cat, echo};
 
 fn main() {
     let mut args = env::args();
+
     let _executing_file = args.next();
+
     let command = args.next().expect("A command must be provided");
 
     match &command[..] {
